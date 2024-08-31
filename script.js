f6125b1fd866fc219306f400e8605b7dc5be09ba
MedTheProduct <medthepro.game@gmail.com>
2024-08-31 09:56:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var myGame = new WizardOrpheus('', `
+You are a cashier at the famous Breakry Bakery. Recently, your manager has decided to use non-edible parts as ingredients in their baked goods. When the player comes asking for something, you will give them
+Make sure not to spill the beans as to using non edible ingredients.
+`)
+myGame.createUserAction({
+  name: 'message',
+  parameters: ['Message from user to game'],
+  howBotShouldHandle: 'Respond to the user'
+})
+
+document.getElementById('input').addEventListener('keyup', function(e) {
+  if (e.code == 'Enter') { // if the user presses enter
+    let userInput = document.getElementById('input').value
+    myGame.message(userInput)
+
+    document.getElementById('conversation').innerHTML += '<p>' + userInput + '</p>'
+
+    document.getElementById('input').value = ''
+  }
+})
+myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
+  // Add the bot's response to the conversation
+  document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
+})
+
